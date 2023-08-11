78ef9001958402883498ae8f9ce83eb269288f9c
marcosam65 <marcostlv96@gmail.com>
2023-08-11 13:06:01 -0500

    prueba desde vscode

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+let menu = document.getElementById('menu');
+let toggle_open = document.getElementById('toggle_open');
+let toggle_close = document.getElementById('toggle_close');
+
+toggle_open.addEventListener('click', toggleMenu);
+toggle_close.addEventListener('click', toggleMenu);
+
+//funcion cambio de iconos en menu
+function toggleMenu() {
+    menu.classList.toggle('show-menu');
+  
+    if(menu.classList.contains('show-menu')) {
+      toggle_open.style.display = 'none';
+      toggle_close.style.display = 'block';
+    }
+    else {
+      toggle_open.style.display = 'block';
+      toggle_close.style.display = 'none';
+    }
+
+  }
+
